d42da8f64a838fcce06114591e66c13f52ac9d6a
rkruggel <rkruggel@bbf7.de>
2023-03-20 19:01:12 +0100

    Das Grundgerüst steht

--- Server/app.py
+++ Server/app.py
@@ -48,7 +48,7 @@ login_manager.login_view = "login"      # die Route Classname
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return Users.query.get(int(user_id))
 
 db.init_app(app)
 
@@ -83,7 +83,6 @@ db.init_app(app)
 
 # Erstellt die Datenbank
 with app.app_context():
-    db.drop_all()
     db.create_all() 
 
 
@@ -96,6 +95,20 @@ def index():
     if current_user.is_active:
         return render_template("index.html", user=current_user.username)
     return render_template("index.html")
+
+# Route
+# Melden
+@app.route("/melden")
+#@login_required
+def melden():
+    return render_template("melden.html", info="n/a")
+
+# Route
+# Hilfe rufen
+@app.route("/hilferufen")
+#@login_required
+def hilferufen():
+    return render_template("hilferufen.html", info="n/a")
 
 # Route
 # Register
@@ -129,8 +142,7 @@ def login():
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    pass
-    return render_template("dashboard.html", fullname=current_user.fullname, user=current_user.username)
+    return render_template("dashboard.html", vorname=current_user.vorname, nachname=current_user.nachname, user=current_user.username)
 
 
 # Route
@@ -142,15 +154,15 @@ def logout():
     return redirect(url_for("index"))
 
 
-
-
-
 # demodaten einfügen
-@app.route("/AddDemodata")
-def demodaten():
+@app.route("/adddemodata")
+def adddemodata():
     '''
       Demodaten einfügen
     '''
+    with app.app_context():
+        db.drop_all()
+        db.create_all() 
 
     #print (date.today())
     # Userdaten erzeugen wenn keine vorhanden sind
@@ -159,52 +171,66 @@ def demodaten():
         newdata = Users(id=100, username="rkruggel", password=get_hashed_pw("12"), 
                         vorname="Roland", nachname="Kruggel", strasse="Beverstr 12", ort="58553 Halver",
                         email="rkruggel@bbf7.de", geboren=date(1959, 1, 2), aktiv=True, 
-                        telefon="02353 6699940", handy="0174 2170044", admin=True, 
-                        created=datetime.now(), updated=datetime.now() )
+                        telefon="02353 6699940", handy="0174 2170044", admin=True  
+                        )
         db.session.add(newdata)
         newdata = Users(id=101, username="pkruggel", password=get_hashed_pw("12"), 
                         vorname="Petra", nachname="Kruggel", strasse="Beverstr 12", ort="58553 Halver",
                         email="pkruggel@gmx.de", geboren=date(1954, 2, 3), aktiv=True, 
-                        telefon="02353 6699940", handy="01525 2637011", admin=False, 
-                        created=datetime.now(), updated=datetime.now() )        
+                        telefon="02353 6699940", handy="01525 2637011", admin=False 
+                        )        
         db.session.add(newdata)
         newdata = Users(id=102, username="tkarrer", password=get_hashed_pw("12"), 
                         vorname="Tanja", nachname="Orthen", strasse="", ort="",
                         email="orthtaja@gmx.de", geboren=date(1989, 3, 12), aktiv=False, 
-                        telefon="02183 3269586", handy="0176 39153897", admin=False, 
-                        created=datetime.now(), updated=datetime.now() )
+                        telefon="02183 3269586", handy="0176 39153897", admin=False  
+                        )
+        db.session.add(newdata)
+        newdata = Users(id=103, username="roland", password=get_hashed_pw("12"), 
+                        vorname="roland", nachname="kruggel", strasse="Testweg 12", ort="Testort",
+                        email="rkruggel@gmx.de", geboren=date(1959, 1, 1), aktiv=True, 
+                        telefon="02353 123456", handy="0174 1234567", admin=True 
+                        )
         db.session.add(newdata)
         db.session.commit()
 
     co = Messages.query.count()
     if co == 0:
-        newdata = Messages(user_id=100, timeAblauf=25*60, infotype="sms", aktiv=True, 
-                           handy="0174 2541225", text="Schnell schnell" )
-        db.session.add(newdata)
-        newdata = Messages(user_id=100, timeAblauf=26*60, infotype="sms", aktiv=True, 
-                           handy="0174 51254124", text="avanti, es eilt" )
-        db.session.add(newdata)
-        newdata = Messages(user_id=100, timeAblauf=30*60, infotype="sms", aktiv=False, 
-                           handy="0174 1441522", text="Hilfe Polizei" )
-        db.session.add(newdata)
-        newdata = Messages(user_id=101, timeAblauf=15*60, infotype="sms", aktiv=True, 
-                           handy="0174 1985214", text="Schnell bei Oma melden" )
-        db.session.add(newdata)
+        db.session.add(Messages(user_id=100, timeAblauf=25*60, infotype="sms", aktiv=True, 
+                                handy="0174 2541225", text="Schnell schnell" ) )
+        db.session.add(Messages(user_id=100, timeAblauf=26*60, infotype="sms", aktiv=True, 
+                                handy="0174 51254124", text="avanti, es eilt" ) )
+        db.session.add(Messages(user_id=100, timeAblauf=30*60, infotype="sms", aktiv=False, 
+                                handy="0174 1441522", text="Hilfe Polizei" ) )
+        db.session.add(Messages(user_id=101, timeAblauf=15*60, infotype="sms", aktiv=True, 
+                                handy="0174 1985214", text="Schnell bei Oma melden" ) )
         db.session.commit()
 
     co = Lebens.query.count()
     if co == 0:
-        newdata = Lebens(user_id=100, zeitstempel=datetime.now(), koordinate="51.0010, 23.9389" )
-        db.session.add(newdata)
-        newdata = Lebens(user_id=101, zeitstempel=datetime.now(), koordinate="51.0010, 23.9389" )
-        db.session.add(newdata)
+        db.session.add(Lebens(user_id=100, zeitstempel=datetime.now(), koordinate="51.0010, 23.9389" ))
+        db.session.add(Lebens(user_id=101, zeitstempel=datetime.now(), koordinate="51.0010, 23.9389" ))
         db.session.commit()
 
 
-    return ("Erg: " + datetime.now().strftime('%d.%m/.%Y %H:%M:%S'))
+    return render_template("info.html", infotext="Info: Datenbank zurückgesetzt: " + datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
 
 
-#demodaten()
+# Route
+# User Data
+@app.route("/userdata")
+#@login_required
+def userdata():
+    return render_template("admin/userdata.html", info="n/a")
+
+# Route
+# Alarmzeiten
+@app.route("/alarmzeiten")
+#@login_required
+def alarmzeiten():
+    return render_template("admin/alarmzeiten.html", info="n/a")
+
+
 
 
 # programmstart für die Entwicklung
--- Server/rkdb/db.py
+++ Server/rkdb/db.py
@@ -2,17 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 #from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_login import UserMixin
 
+from datetime import datetime
+
 db = SQLAlchemy()
 
 
 # Model
+# Abstract
+class TimestampModel(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated = db.Column(db.DateTime, onupdate=datetime.now)
+
+
+# Model
 # User
-class Users(db.Model, UserMixin):
+#class Users(db.Model, UserMixin):
+class Users(TimestampModel, UserMixin):
     '''
       Die Adresse des Users.
     '''
     #__tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     vorname = db.Column(db.String)
@@ -25,34 +37,42 @@ class Users(db.Model, UserMixin):
     telefon = db.Column(db.String)
     handy = db.Column(db.String)
     admin = db.Column(db.Boolean)
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
+    #messages = db.relationship('Messages', backref='users') #, lazy=True)
+    # created = db.Column(db.DateTime)
+    # updated = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return f'Users {"self.username"}'
 
 
 # Model
 # Message
-class Messages(db.Model, UserMixin):
+# class Messages(db.Model, UserMixin):
+class Messages(TimestampModel, UserMixin):
     '''
       Hier wird eingetragen wer in welchen Situaltonen alamiert wird.
     '''
     #__tablename__ = "message"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)     # Verbindung zur Tabelle 'user'
+    # id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) #, nullable=False)  # Verbindung zur Tabelle 'users'
     timeAblauf = db.Column(db.Integer)  # z.B. 26; Wenn sich der User 26 Stunden nicht gemeldet hat wird alarm ausgegeben.
     infotype = db.Column(db.String)     # z.B. SMS, email, handyalarm
     aktiv = db.Column(db.Boolean)
     handy = db.Column(db.String)        # Handynummer die benachrichtigt wird
     text = db.Column(db.String)         # Text der in der benachrichtigung steht
     
+    def __repr__(self) -> str:
+        return f'Messages {"text[:10]"}...'
+    
 # Model
 # Leben
-class Lebens(db.Model, UserMixin):
+# class Lebens(db.Model, UserMixin):
+class Lebens(TimestampModel, UserMixin):
     '''
       Hier wird ein Zeitstempel eingetragen wenn der User sich gemeldet hat.
     '''
     #__tablename__ = "leben"
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)         # Verbindung zur Tabelle 'user'
     zeitstempel = db.Column(db.DateTime)    # Zeitstempel wenn der user sich gemeldet hat
     koordinate = db.Column(db.String)       # die Koordinate wo sich der user bein Zeitstempel befindet
